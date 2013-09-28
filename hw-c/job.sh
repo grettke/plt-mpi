a400e7b1fe7649eee7bad8e0d3c08515d0e60e48
Grant Rettke <grettke@acm.org>
2013-09-28 23:14:31 +0000

    Corrected mpich2 installation and removed the hwc executable

--- hw-c/job.sh
+++ hw-c/job.sh
@@ -7,6 +7,6 @@
 #$ -j y
 #$ -o hwc.$JOB_ID.log
 
-export PATH=$PATH:/opt/MPICH2-1.1/bin
-MPIEXEC="/opt/MPICH2-1.1/bin/mpiexec --n $NSLOTS"
+export PATH=$PATH:/opt/mpich2/gnu/bin
+MPIEXEC="/opt/mpich2/gnu/bin/mpiexec --n $NSLOTS"
 $MPIEXEC hwc
